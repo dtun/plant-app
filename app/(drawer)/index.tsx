ee116c28aaf4811d90bd6fa1a9a7501eac58717d
Danny Tunney <dtun@me.com>
2025-10-05 21:04:33 -0700

    Update keyboard view config

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,6 +1,6 @@
 import { PlantForm } from "@/components/plant-form";
 import { ThemedView } from "@/components/themed-view";
-import { KeyboardAvoidingView, StyleSheet } from "react-native";
+import { KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -8,7 +8,11 @@ export default function HomeScreen() {
   return (
     <ThemedView style={styles.flex1}>
       <SafeAreaView edges={["bottom"]} style={styles.flex1}>
-        <KeyboardAvoidingView behavior="padding" style={styles.flex1}>
+        <KeyboardAvoidingView
+          behavior={Platform.select({ default: "height", ios: "padding" })}
+          keyboardVerticalOffset={Platform.select({ default: 20, ios: 0 })}
+          style={styles.flex1}
+        >
           <ScrollView
             contentContainerStyle={styles.flex1}
             keyboardShouldPersistTaps="handled"
