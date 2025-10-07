12f2b997ccb2337809a58859631120d3938633c5
Danny Tunney <dtun@me.com>
2025-10-07 11:44:43 -0700

    feat: update drawer menu

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,6 +1,7 @@
 import { IconSymbol } from "@/components/ui/icon-symbol";
 import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { Image } from "expo-image";
 import { Drawer } from "expo-router/drawer";
 import React from "react";
@@ -11,6 +12,7 @@ let leafImage = require("@/assets/images/KeepTend-Leaf.png");
 export default function DrawerLayout() {
   let colorScheme = useColorScheme();
   let tintColor = Colors[colorScheme ?? "light"].tint;
+  let textColor = useThemeColor({}, "text");
 
   return (
     <Drawer
@@ -36,10 +38,26 @@ export default function DrawerLayout() {
         }}
       />
       <Drawer.Screen
+        name="share"
+        options={{
+          drawerLabel: "Share",
+          title: "Share",
+          drawerIcon: ({ color }) => (
+            <IconSymbol size={28} name="qrcode.viewfinder" color={color} />
+          ),
+          headerTitle: () => (
+            <Image source={leafImage} style={styles.leafImage} />
+          ),
+          headerShown: true,
+          headerTintColor: textColor,
+        }}
+      />
+      <Drawer.Screen
         name="settings"
         options={{
           drawerLabel: "Settings",
           title: "Settings",
+          drawerItemStyle: { display: "none" },
           drawerIcon: ({ color }) => (
             <IconSymbol size={28} name="gear" color={color} />
           ),
@@ -50,9 +68,11 @@ export default function DrawerLayout() {
         options={{
           drawerLabel: "AI Setup",
           title: "AI Setup",
-          drawerItemStyle: { display: "none" },
+          drawerIcon: ({ color }) => (
+            <IconSymbol size={28} name="wrench" color={color} />
+          ),
           headerShown: true,
-          headerTintColor: tintColor,
+          headerTintColor: textColor,
         }}
       />
     </Drawer>
