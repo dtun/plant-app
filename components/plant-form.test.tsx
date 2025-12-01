4c2ebb8fe3683603a3ed239b25b1831480aa8cdc
Danny Tunney <dtun@me.com>
2025-11-30 20:23:57 -0700

    test(plant-form): simplify title visibility test and update assertion method

--- components/plant-form.test.tsx
+++ components/plant-form.test.tsx
@@ -2,9 +2,8 @@ import { render, screen } from "@testing-library/react-native";
 
 import { PlantForm } from "./plant-form";
 
-describe("PlantForm - Title Positioning", () => {
-  test("title is always visible", () => {
-    render(<PlantForm />);
-    expect(screen.getByText("About your plant")).toBeTruthy();
-  });
+test("title is always visible", () => {
+  render(<PlantForm />);
+
+  expect(screen.getByText("About your plant")).toBeOnTheScreen();
 });
