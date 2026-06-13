154ed752d578ad111f574acb49aed9c1ae1295fa
Danny Tunney <dtun@me.com>
2026-06-13 07:48:05 -0700

    chore: delete haptics mock

--- apps/mobile/__mocks__/haptics.ts
+++
@@ -1,6 +0,0 @@
-export let success = jest.fn();
-export let warning = jest.fn();
-export let error = jest.fn();
-export let light = jest.fn();
-export let medium = jest.fn();
-export let selection = jest.fn();
