ae4df048bdd8ca03e819b3a1249d6da580d6b884
Danny Tunney <dtun@me.com>
2026-07-29 19:14:39 -0400

    style: add header height

--- apps/mobile/app/(drawer)/index.tsx
+++ apps/mobile/app/(drawer)/index.tsx
@@ -1,12 +1,17 @@
 import { PlantForm } from "@/components/plant-form";
+import { useHeaderHeight } from "@react-navigation/elements";
 import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
 
 export default function HomeScreen() {
   let { setOptions } = useNavigation();
+  let headerHeight = useHeaderHeight()
 
   return (
-    <View className="flex-1 bg-background px-4">
+    <View
+      className="flex-1 bg-background px-4"
+      style={{ paddingTop: headerHeight }}
+    >
       <PlantForm setOptions={setOptions} />
     </View>
   );
